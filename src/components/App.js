72941f90791f745c19a5e77da7d1834609b6f68e
ceazb21 <ceazb21@gmail.com>
2021-12-01 21:24:39 -0600

    add global styles to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
-
-
 function App() {
   return (
-    <div className="app">
-      Hello
+    <div>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ea mollitia earum ipsam. Quo magni repudiandae debitis distinctio accusantium culpa mollitia voluptate dolore nobis qui.</p>
+
+      {/* <button type='button' className='btn-1'>See Product</button> */}
+
+
+      <label htmlFor="name" className='text-label'>Name</label>
+      <input type='text' className='text-field' placeholder='Insert your name'></input>
+
+
+
     </div>
   );
 }
